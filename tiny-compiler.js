696aa1c77a14fe778d7bb75f51e2d93ba72453eb
LeeZ1Q <466944567@qq.com>
2023-06-12 21:52:13 +0800

    feat:COMPILER

--- tiny-compiler.js
+++ tiny-compiler.js
@@ -162,26 +162,26 @@ function traverser(ast, visitor) {
 	//遍历节点
 	function traverseNode(node, parent) {
 		//获取对应的方法
-    const method = visitor[node.type];
-    //调用对应的方法
-    if(method && method.enter){
-      method.enter(node, parent);
-    }
+		const method = visitor[node.type];
+		//调用对应的方法
+		if (method && method.enter) {
+			method.enter(node, parent);
+		}
 
-    //继续遍历
-    switch(node.type){
-      case 'Program':
-        traverseArray(node.body, node);
-        break;
-      case 'CallExpression':
-        traverseArray(node.params, node);
-        break;
-      case 'NumberLiteral':
-      case 'StringLiteral':
-        break;
-      default:
-        throw new TypeError(node.type);
-    }
+		//继续遍历
+		switch (node.type) {
+			case "Program":
+				traverseArray(node.body, node);
+				break;
+			case "CallExpression":
+				traverseArray(node.params, node);
+				break;
+			case "NumberLiteral":
+			case "StringLiteral":
+				break;
+			default:
+				throw new TypeError(node.type);
+		}
 	}
 
 	//开始遍历
@@ -189,104 +189,121 @@ function traverser(ast, visitor) {
 }
 
 function transformer(ast) {
-  const newAst = {
-    type: 'Program',
-    body: []
-  };
+	const newAst = {
+		type: "Program",
+		body: [],
+	};
 
-  // 因为visitor的内部并没有和newAst建立链接，但和旧的AST的node和parent node都有链接
-  // 所以这里可以偷懒，用旧AST的一个字段储存newAst的一个引用，这样就可以通过旧AST来访问和修改newAst了
-  ast._context = newAst.body;
+	// 因为visitor的内部并没有和newAst建立链接，但和旧的AST的node和parent node都有链接
+	// 所以这里可以偷懒，用旧AST的一个字段储存newAst的一个引用，这样就可以通过旧AST来访问和修改newAst了
+	ast._context = newAst.body;
 
-  //遍历
-  traverser(ast, {
-    //定义visitor的方法
+	//遍历
+	traverser(ast, {
+		//定义visitor的方法
 
-    //处理NumberLiteral
-    NumberLiteral: {
-      enter(node, parent) {
-        parent._context.push({
-          type: 'NumberLiteral',
-          value: node.value
-        });
-      }
-    },
+		//处理NumberLiteral
+		NumberLiteral: {
+			enter(node, parent) {
+				parent._context.push({
+					type: "NumberLiteral",
+					value: node.value,
+				});
+			},
+		},
 
-    //处理StringLiteral
-    StringLiteral: {
-      enter(node, parent) {
-        parent._context.push({
-          type: 'StringLiteral',
-          value: node.value
-        });
-      }
-    },
+		//处理StringLiteral
+		StringLiteral: {
+			enter(node, parent) {
+				parent._context.push({
+					type: "StringLiteral",
+					value: node.value,
+				});
+			},
+		},
 
-    //处理CallExpression
-    CallExpression: {
-      enter(node, parent) {
-        //创建一个新的节点，类型为CallExpression
-        let expression = {
-          type: 'CallExpression',
-          callee: {
-            type: 'Identifier',
-            name: node.name
-          },
-          arguments: [],
-        };
+		//处理CallExpression
+		CallExpression: {
+			enter(node, parent) {
+				//创建一个新的节点，类型为CallExpression
+				let expression = {
+					type: "CallExpression",
+					callee: {
+						type: "Identifier",
+						name: node.name,
+					},
+					arguments: [],
+				};
 
-        //同样为了方便修改newAst
-        node._context = expression.arguments;
+				//同样为了方便修改newAst
+				node._context = expression.arguments;
 
-        //如果父节点不是CallExpression
-        //那么就把父节点包在一个ExpressionStatement节点中
-        if(parent.type !== 'CallExpression'){
-          expression = {
-            type: 'ExpressionStatement',
-            expression: expression
-          };
-        }
+				//如果父节点不是CallExpression
+				//那么就把父节点包在一个ExpressionStatement节点中
+				if (parent.type !== "CallExpression") {
+					expression = {
+						type: "ExpressionStatement",
+						expression: expression,
+					};
+				}
 
-        //把CallExpression添加到父节点的context中
-        parent._context.push(expression);
-      }
-    }
-  });
+				//把CallExpression添加到父节点的context中
+				parent._context.push(expression);
+			},
+		},
+	});
 
-  return newAst;
+	return newAst;
 }
 
 //CODEGENERATOR
 function codeGenerator(node) {
-  switch(node.type){
-    //如果是Program，那么就遍历它的body属性，然后用换行符连接每个节点的结果
-    case 'Program':
-      return node.body.map(codeGenerator).join('\n');
-    
-    //如果是ExpressionStatement，那么就返回它的expression属性，并且在后面加上分号
-    case 'ExpressionStatement':
-      return codeGenerator(node.expression) + ';';
+	switch (node.type) {
+		//如果是Program，那么就遍历它的body属性，然后用换行符连接每个节点的结果
+		case "Program":
+			return node.body.map(codeGenerator).join("\n");
 
-    //如果是CallExpression，那么就返回它的callee属性，然后添加一个左括号，接着遍历它的arguments属性，用逗号连接每个节点的结果，最后加上一个右括号
-    case 'CallExpression':
-      return (
-        codeGenerator(node.callee) + '(' + node.arguments.map(codeGenerator).join(', ') + ')'      
-      );
+		//如果是ExpressionStatement，那么就返回它的expression属性，并且在后面加上分号
+		case "ExpressionStatement":
+			return codeGenerator(node.expression) + ";";
 
-    //如果是Identifier，那么就返回它的name属性
-    case 'Identifier':
-      return node.name;
-    
-    //如果是NumberLiteral，那么就返回它的value属性
-    case 'NumberLiteral':
-      return node.value;
-    
-    //如果是StringLiteral，那么就在它的value属性的两边加上双引号
-    case 'StringLiteral':
-      return '"' + node.value + '"';
-    
-    //如果没有匹配到，就抛出错误
-    default:
-      throw new TypeError(node.type);
-  }
+		//如果是CallExpression，那么就返回它的callee属性，然后添加一个左括号，接着遍历它的arguments属性，用逗号连接每个节点的结果，最后加上一个右括号
+		case "CallExpression":
+			return (
+				codeGenerator(node.callee) +
+				"(" +
+				node.arguments.map(codeGenerator).join(", ") +
+				")"
+			);
+
+		//如果是Identifier，那么就返回它的name属性
+		case "Identifier":
+			return node.name;
+
+		//如果是NumberLiteral，那么就返回它的value属性
+		case "NumberLiteral":
+			return node.value;
+
+		//如果是StringLiteral，那么就在它的value属性的两边加上双引号
+		case "StringLiteral":
+			return '"' + node.value + '"';
+
+		//如果没有匹配到，就抛出错误
+		default:
+			throw new TypeError(node.type);
+	}
 }
+
+//COMPILER
+function compiler(input) {
+	const tokens = tokenizer(input);
+	const ast = parser(tokens);
+	const newAst = transformer(ast);
+
+	return codeGenerator(newAst);
+}
+
+// TEST
+console.log(compiler('(add 10 (subtract 20 100)) (connect "Hello" "World")'));
+// add(10, subtract(20, 100));
+// connect("Hello", "World");
